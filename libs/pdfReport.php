75c7c2ea8134d6c258022557be62bd3d66d39b13
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-04-16 01:25:40 +0200

    Logo Größe

--- libs/pdfReport.php
+++ libs/pdfReport.php
@@ -33,7 +33,7 @@ trait pdfReport
         <img src="@$logoData">
     </td>
 	<td align="right">
-        <img src="@$logoBafaData">
+        <img style="max-width: 300px; height: auto; " src="@$logoBafaData">
 		<br/>
 		$date
 	</td>
