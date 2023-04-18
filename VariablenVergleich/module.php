3bf24be5f211d1d3398f8668a94f278606b1455d
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-04-18 11:37:14 +0200

    Debug keysValueNull

--- VariablenVergleich/module.php
+++ VariablenVergleich/module.php
@@ -360,6 +360,8 @@ include_once __DIR__ . '/../libs/pdfReport.php';
 
                 //Filter Werte mit 0
                 $keysValueNull = (array_keys($valuesY, 0));
+                IPS_LogMessage('keysValueNull', print_r($keysValueNull, true));
+                
                 for ($i = 0; $i < count($valuesY) - 1; $i++) {
                     unset($valuesX[$i]);
                     unset($valuesY[$i]);
